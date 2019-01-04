7f96f974d054602b6ccf76354cf63b939cafd481
Miguel Hernández <miguel.hernandez@payworks.com>
2019-01-04 12:55:37 +0100

    Implement Spring Boot actuator and TCP Mocker application

---
+++ tcp-mocker-web/src/main/java/io/payworks/labs/tcpmocker/TCPMockerApplication.java
@@ -0,0 +1,13 @@
+package io.payworks.labs.tcpmocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TCPMockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TCPMockerApplication.class);
+    }
+
+}
---
+++ tcp-mocker-web/src/main/java/io/payworks/labs/tcpmocker/environment/AliasWebConfig.java
@@ -0,0 +1,32 @@
+package io.payworks.labs.tcpmocker.environment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static java.lang.String.format;
+
+@Configuration
+public class AliasWebConfig implements WebMvcConfigurer {
+
+    private WebEndpointProperties actuatorWebProperties;
+
+    public AliasWebConfig(@Autowired(required = false) WebEndpointProperties actuatorWebProperties) {
+        this.actuatorWebProperties = actuatorWebProperties;
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        if (actuatorWebProperties != null) {
+            String actuatorPrefix = actuatorWebProperties.getBasePath();
+
+            registry.addViewController("/health").setViewName(format("forward:%s/health", actuatorPrefix));
+            registry.addViewController("/info").setViewName(format("forward:%s/info", actuatorPrefix));
+            registry.addViewController("/prometheus").setViewName(format("forward:%s/prometheus", actuatorPrefix));
+            registry.addViewController("/metrics").setViewName(format("forward:%s/metrics", actuatorPrefix));
+        }
+    }
+
+}
