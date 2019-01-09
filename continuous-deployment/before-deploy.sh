552599c91d4bbdf15fbb662f68b62145c6cf26d0
Matej Risek <matej.risek@payworks.com>
2019-01-09 13:22:58 +0100

    Fix before-deploy script

--- continuous-deployment/before-deploy.sh
+++ continuous-deployment/before-deploy.sh
@@ -4,8 +4,8 @@ set -x
 
 # Decrypt signing key
 openssl aes-256-cbc -K $encrypted_a3a4db86818a_key -iv $encrypted_a3a4db86818a_iv \
-  -in payworks_sign_key.asc.enc -out ~\/temp/tcp-mocker-keys/payworks_sign_key.asc \
+  -in payworks_sign_key.asc.enc -out continuous-deployment/payworks_sign_key.asc \
   -d
 
 # Import signing key
-gpg --fast-import ~\/temp/tcp-mocker-keys/payworks_sign_key.asc
+gpg --fast-import continuous-deployment/payworks_sign_key.asc
