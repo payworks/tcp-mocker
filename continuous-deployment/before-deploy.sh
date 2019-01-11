3aacc57034c8359b7c890b1fb20af2e839098267
Matej Risek <matej.risek@payworks.com>
2019-01-11 09:31:24 +0100

    Travis.com

--- continuous-deployment/before-deploy.sh
+++ continuous-deployment/before-deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 # Decrypt signing key
 openssl aes-256-cbc -K $encrypted_a3a4db86818a_key -iv $encrypted_a3a4db86818a_iv \
   -in continuous-deployment/payworks_sign_key.asc.enc -out continuous-deployment/payworks_sign_key.asc \
