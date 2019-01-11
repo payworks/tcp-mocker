425f18af77be4a9a3c3c9677f94c8923518262ab
Matej Risek <matej.risek@payworks.com>
2019-01-11 10:46:17 +0100

    Code review

--- continuous-deployment/before-deploy.sh
+++ continuous-deployment/before-deploy.sh
@@ -2,8 +2,4 @@
 
 # Decrypt signing key
 openssl aes-256-cbc -K $encrypted_a3a4db86818a_key -iv $encrypted_a3a4db86818a_iv \
-  -in continuous-deployment/payworks_sign_key.asc.enc -out continuous-deployment/payworks_sign_key.asc \
-  -d
-
-# Import signing key
-gpg --fast-import continuous-deployment/payworks_sign_key.asc
+  -in continuous-deployment/payworks_sign_key.asc.enc -d | gpg --fast-import
