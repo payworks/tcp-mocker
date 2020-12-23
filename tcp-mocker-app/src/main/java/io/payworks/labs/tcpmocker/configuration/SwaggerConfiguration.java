fee004514837a8820031ceef110d784e14dad290
Andrey Saksonov <asaksono@visa.com>
2020-12-23 17:15:57 +0300

    rollback springfox upgrade

--- tcp-mocker-app/src/main/java/io/payworks/labs/tcpmocker/configuration/SwaggerConfiguration.java
+++ tcp-mocker-app/src/main/java/io/payworks/labs/tcpmocker/configuration/SwaggerConfiguration.java
@@ -10,7 +10,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.ServletContext;
 import java.net.URI;
@@ -18,7 +18,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
