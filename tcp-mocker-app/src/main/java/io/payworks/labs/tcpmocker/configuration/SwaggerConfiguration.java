b93ad2817c953f8ed6e5bc83c54b1c2f73191445
Andrey Saksonov <asaksono@visa.com>
2020-12-23 14:43:09 +0300

    change swagger annotation

--- tcp-mocker-app/src/main/java/io/payworks/labs/tcpmocker/configuration/SwaggerConfiguration.java
+++ tcp-mocker-app/src/main/java/io/payworks/labs/tcpmocker/configuration/SwaggerConfiguration.java
@@ -10,7 +10,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import javax.servlet.ServletContext;
 import java.net.URI;
@@ -18,7 +18,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfiguration {
 
     @Bean
